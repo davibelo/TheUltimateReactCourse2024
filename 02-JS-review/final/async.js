24b6703d370101886ac2f59b633c9f6e07ceb273
Davi Belo <davibelo@gmail.com>
2024-12-19 14:50:59 -0300

    async explanation

--- 02-JS-review/final/async.js
+++ 02-JS-review/final/async.js
@@ -1,21 +1,27 @@
-// Asynchronous Javascript: Promisses
+// JavaScript has a non-blocking Nature
+// Its execute line after line of code without waiting for the previous line to complete
 
-// Using promises
-fetch("https://jsonplaceholder.typicode.com/todos")
-  .then((res) => res.json())
-  .then((data) => console.log(data));
+// Using .then()
+function getTodos0() {
+  return fetch("https://jsonplaceholder.typicode.com/todos").then((res) =>
+    res.json()
+  );
+}
 
+const data1 = await getTodos0();
+console.log(data1);
 console.log("davi");
 
-// async call
-async function getTodos() {
+// Using async function with await
+// need to use async before the function
+async function getTodos1() {
   const res = await fetch("https://jsonplaceholder.typicode.com/todos");
   const data = await res.json();
-  console.log(data);
   return data;
 }
 
-getTodos();
+const data2 = await getTodos1();
+console.log(data2);
 console.log("davi");
 
 // both will print the same thing, but async/await is more readable than promises
