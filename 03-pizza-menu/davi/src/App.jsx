9d58100a0abb7588e00d3353a704440d0a0fe7fd
Davi Belo <davibelo@gmail.com>
2024-12-19 22:05:27 -0300

    starting pizza

--- 03-pizza-menu/davi/src/App.jsx
+++ 03-pizza-menu/davi/src/App.jsx
@@ -1,5 +1,35 @@
+import pizzaData from "./data";
+
 function App() {
-  return <h1>Hello React!!</h1>;
+  return (
+    <div>
+      <h1>Pizza App</h1>
+      <Pizza />
+    </div>
+  );
 }
+
+function Pizza() {
+  return (
+    <div>
+      <img src="/pizzas/focaccia.jpg" alt="Focaccia" />
+      <p>Name: {pizzaData[0].name}</p>
+      <p>Ingredients: {pizzaData[0].ingredients}</p>
+    </div>
+  );
+}
+
+const PizzaList = () => {
+  return (
+    <div>
+      <h1>Pizza Menu</h1>
+      <ul>
+        {pizzaData.map((pizza, index) => (
+          <li key={index}>{pizza.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 export default App;
