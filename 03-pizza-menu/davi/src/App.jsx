5bba9b1fc4b4e56ffad0bae8a3308b915d3adc5d
Davi Belo <davibelo@gmail.com>
2024-12-19 23:36:30 -0300

    alert

--- 03-pizza-menu/davi/src/App.jsx
+++ 03-pizza-menu/davi/src/App.jsx
@@ -3,9 +3,47 @@ import pizzaData from "./data";
 function App() {
   return (
     <div>
-      <h1>Pizza App</h1>
+      <Header />
+      <Menu />
+      <Footer />
+    </div>
+  );
+}
+
+function Header() {
+  return (
+    <header>
+      <h1>Belo's Pizza</h1>
+    </header>
+  );
+}
+
+function Menu() {
+  return (
+    <div>
+      <h2>Our Menu</h2>
+      <Pizza />
+      <Pizza />
       <Pizza />
     </div>
+  );
+}
+
+function Footer() {
+  const hour = new Date().getHours();
+  console.log(hour);
+  const openHour = 9;
+  const closeHour = 23;
+  const isOpen = hour > openHour && hour <= closeHour;
+
+  if (isOpen) alert("We're currently open");
+  else alert("Sorry, we're currently closed");
+
+  return (
+    <footer>
+      <p>{new Date().toLocaleTimeString()}</p>
+      <p>We're currently open</p>
+    </footer>
   );
 }
 
