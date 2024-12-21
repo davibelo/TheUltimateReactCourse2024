18a2dd509b203a0d622af00a4ffd0886f8babb33
Davi Belo <davibelo@gmail.com>
2024-12-20 23:17:10 -0300

    ok

--- 03-pizza-menu/davi/src/App.jsx
+++ 03-pizza-menu/davi/src/App.jsx
@@ -30,6 +30,10 @@ function Menu() {
   return (
     <main className="menu">
       <h2>Our Menu</h2>
+      <p>
+        Authentic Italian cuisine. 6 creative dishes to choose from. All from
+        our stone oven, all organic, all delicious.
+      </p>
       <ul className="pizzas">
         {pizzaData.map((pizza, index) => (
           <Pizza pizzaObj={pizza} key={index} />
